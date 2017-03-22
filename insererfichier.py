b870c4b051b9ca7429d618e86651c04f5a5d6c5a
Jeremie DURAND <E155245U@U-INF-J-C002-16.iut-nantes.univ-nantes.prive>
2017-03-22 16:15:55 +0100

    Automatisation de l'automate

--- insererfichier.py
+++ insererfichier.py
@@ -1,7 +1,7 @@
 import sqlite3
 import csv
 
-conn = sqlite3.connect('/hometu/etudiants/b/l/E155781C/workspace/Semestre4/Git/Python_TD2/BD/TD2_python.db');
+conn = sqlite3.connect('/hometu/etudiants/d/u/E155245U/Semestre4/Git/Python_TD2/BD/TD2_python.db');
 
 cur = conn.cursor()
 
@@ -10,15 +10,21 @@ liste = []
 #conn.execute("CREATE TABLE equipements_activites(ComInsee double,ComLib text,EquipementId double,EquNbEquIdentique double,ActCode double,ActLib text,EquActivitePraticable text,EquActivitePratique text,EquActiviteSalleSpe text,ActNivLib text)")
 #conn.execute("CREATE TABLE installations_table(NomInstallation text, NumInstallation text, NomCommune text, CodeINSEE text, CodePostal text, NomLieuDit text, NumeroVoie text, NomVoie text,location text, Longitude text, Latitude text, AucunAmenagementAccessibilite text, AccessibiliteHandicapesMobiliteReduite text, AccessibiliteHandicapesSensoriels text, EmpriseFonciere text, LogementGardien text, MultiCommune text, NbrTotalParking text, NbrTotalParkingHandicapes text, InstallationParticuliere text, DesserteMetro text, DesserteBus text, DesserteTram text,DesserteTrain text, DesserteBateau text, DesserteAutre text, NombreTotalEquipementsSportifs text, NombreTotalFichesEquipements text, DateMajFicheInstallation text)")
 
+with open('/hometu/etudiants/d/u/E155245U/Semestre4/Git/Python_TD2/CSV/J334_equipements_activites.csv', 'r') as csvfile:
+    reader = csv.reader(csvfile, delimiter=',', quotechar='*')
+    for row in reader:
+        liste.append(row)
+        print(liste[0])
 
-with open('/hometu/etudiants/b/l/E155781C/workspace/Semestre4/Git/Python_TD2/CSV/J334_equipements_activites.csv', 'r') as csvfile:
+
+with open('/hometu/etudiants/d/u/E155245U/Semestre4/Git/Python_TD2/CSV/J334_equipements_activites.csv', 'r') as csvfile:
     reader = csv.reader(csvfile, delimiter=',', quotechar='*')
     for row in reader:
         liste.append(row)
         
         cur.execute("INSERT INTO equipements_activites VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", (row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7], row[8], row[9]))
 
-with open('/hometu/etudiants/b/l/E155781C/workspace/Semestre4/Git/Python_TD2/CSV/23440003400026_J335_installations_table.csv', 'r') as csvfile:
+with open('/hometu/etudiants/d/u/E155245U/Semestre4/Git/Python_TD2/CSV/23440003400026_J335_installations_table.csv', 'r') as csvfile:
     reader = csv.reader(csvfile, delimiter=',', quotechar='*')
     for row in reader:
         liste.append(row)
